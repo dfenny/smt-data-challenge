8c22670ddb9362a0aa52ac14de9766c701f455ac
kruth99 <36120884+kruth99@users.noreply.github.com>
2024-08-04 18:05:11 -0600

    Anova Results
    Created a table called anova_results that showcases the variable with the lowest pvalue and if it is significant.

--- kens_analysis/batter_pitcher_hits.R
+++ kens_analysis/batter_pitcher_hits.R
@@ -12,6 +12,7 @@ pitcher_pitch_info <- read.csv("../dougs_analysis/pitcher-location.csv") |>
 batter_pitcher_hits <- merge(x = batter_pitch_info, y = pitcher_pitch_info, by = c("game_str", "play_per_game")) 
   
 batter_pitcher_hits <- transform(batter_pitcher_hits, pitcher_hand = ifelse(pitcher_is_righty, "R", "L"))
+  
 
 write.csv(batter_pitcher_hits, "batter_pitcher_hits.csv")
 
@@ -27,25 +28,106 @@ batter_pitcher_outcomes <- batter_pitcher_hits |>
   ) |>
   ungroup()
 
+
+
 write.csv(batter_pitcher_outcomes, "batter_pitcher_outcomes.csv")
 
 plot1 <- batter_pitcher_hits |>
   filter(batter_hand == "L", pitcher_is_righty == "False") |>
-  ggplot(aes(x = ball_position_x, y = ball_position_y)) + geom_point()
+  ggplot(aes(x = ball_position_x, y = ball_position_y)) + 
+  geom_point() +
+  ggtitle("Left Handed Batter vs Left Handed Pitcher") +
+  xlab("X Coordinate") +
+  ylab("Y Coordinate") +
+  theme(plot.title = element_text(hjust = 0.5))
 plot2 <- batter_pitcher_hits |>
   filter(batter_hand == "R", pitcher_is_righty == "False") |>
-  ggplot(aes(x = ball_position_x, y = ball_position_y)) + geom_point()
+  ggplot(aes(x = ball_position_x, y = ball_position_y)) + 
+  geom_point() +
+  ggtitle("Right Handed Batter vs Left Handed Pitcher") +
+  xlab("X Coordinate") +
+  ylab("Y Coordinate") +
+  theme(plot.title = element_text(hjust = 0.5))
 plot3 <- batter_pitcher_hits |>
   filter(batter_hand == "L", pitcher_is_righty == "True") |>
-  ggplot(aes(x = ball_position_x, y = ball_position_y)) + geom_point()
+  ggplot(aes(x = ball_position_x, y = ball_position_y)) + 
+  geom_point() +
+  ggtitle("Right Handed Batter vs Right Handed Pitcher") + 
+  xlab("X Coordinate") +
+  ylab("Y Coordinate") +
+  theme(plot.title = element_text(hjust = 0.5))
 plot4 <- batter_pitcher_hits |>
   filter(batter_hand == "R", pitcher_is_righty == "True") |>
-  ggplot(aes(x = ball_position_x, y = ball_position_y)) + geom_point()
-
+  ggplot(aes(x = ball_position_x, y = ball_position_y)) + 
+  geom_point() +
+  ggtitle("Right Handed Batter vs Right Handed Pitcher") + 
+  xlab("X Coordinate") +
+  ylab("Y Coordinate") +
+  theme(plot.title = element_text(hjust = 0.5))
 (plot1 + plot2) / (plot3 + plot4)
 
 #ANOVA
-pitcher_vs_batter <- pitcher_vs_batter
+batter_ids <- unique(batter_pitcher_hits$batter)
+for(id in batter_ids) {
+  
+  plot1 <- batter_pitcher_hits |>
+    filter(batter == id, batter_hand == "L", pitcher_is_righty == "False") |>
+    ggplot(aes(x = ball_position_x, y = ball_position_y)) + 
+    geom_point() +
+    ggtitle("Left Handed Batter vs Left Handed Pitcher") +
+    xlab("X Coordinate") +
+    ylab("Y Coordinate") +
+    theme(plot.title = element_text(hjust = 0.5))
+  plot2 <- batter_pitcher_hits |>
+    filter(batter == id, batter_hand == "R", pitcher_is_righty == "False") |>
+    ggplot(aes(x = ball_position_x, y = ball_position_y)) + 
+    geom_point() +
+    ggtitle("Right Handed Batter vs Left Handed Pitcher") +
+    xlab("X Coordinate") +
+    ylab("Y Coordinate") +
+    theme(plot.title = element_text(hjust = 0.5))
+  plot3 <- batter_pitcher_hits |>
+    filter(batter == id, batter_hand == "L", pitcher_is_righty == "True") |>
+    ggplot(aes(x = ball_position_x, y = ball_position_y)) +
+    geom_point() +
+    ggtitle("Left Handed Batter vs Right Handed Pitcher") + 
+    xlab("X Coordinate") +
+    ylab("Y Coordinate") +
+    theme(plot.title = element_text(hjust = 0.5))
+  plot4 <- batter_pitcher_hits |>
+    filter(batter == id, batter_hand == "R", pitcher_is_righty == "True") |>
+    ggplot(aes(x = ball_position_x, y = ball_position_y)) + 
+    geom_point() +
+    ggtitle("Right Handed Batter vs Right Handed Pitcher") +
+    xlab("X Coordinate") +
+    ylab("Y Coordinate") +
+    theme(plot.title = element_text(hjust = 0.5))
+  print((plot1 + plot2) / (plot3 + plot4))
+}
+for(id in batter_ids) {
+  temp_hits <- batter_pitcher_hits |>
+    filter(batter == id)
+  batter_pitch_aov <- NULL
+  if(nrow(temp_hits ) > 30) {
+    if(n_distinct(temp_hits$batter_hand) > 1) {
+      batter_pitch_aov <-  aov(ball_dist ~ batter_hand * push_pull * pitcher_hand, data = temp_hits)
+    }
+    else if((n_distinct(temp_hits$batter_hand) == 1) & (n_distinct(temp_hits$pitcher_hand) == 1)) {
+      batter_pitch_aov <-  aov(ball_dist ~  push_pull, data = temp_hits)
+    }
+    else {
+      batter_pitch_aov <-  aov(ball_dist ~  push_pull * pitcher_hand, data = temp_hits)
+    }
+    if(!is.null(batter_pitch_aov)) {
+      print(summary(batter_pitch_aov)[[1]][["Pr(>F)"]])
+      print(min(summary(batter_pitch_aov)[[1]][["Pr(>F)"]], na.rm=TRUE))
+    }
+    else(
+      print("Not Enough ABs")
+    )
+  }
+}
 
 
-test <-  aov(ball_dist ~ batter_hand * push_pull * pitcher_hand, data = pitcher_vs_batter)
+
+
